237096c416377c18796ec79b5a8ae7005a21c752
David Abril <david_aabril@proton.me>
2025-11-28 17:37:11 -0500

    schemas & API postman

--- books-api/main.py
+++ books-api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from . import schemas
 
 app = FastAPI()
 
 @app.get("/")
 def get_root():
     return "Welcome to the Books API"
+
+@app.post("/book/")
+def create_book(request: schemas.BookAuthorPayload):
+    return "New book created " + request.book.title + " " + str(request.book.number_of_pages) + \
+           " by " + request.author.first_name + " " + request.author.last_name
